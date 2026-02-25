359dfbe1c2b5403ee9dd520eee02136bad4907f0
Yevink15 <yevinkariyawasam@gmail.com>
2026-02-24 23:05:08 -0500

    Fix data.js parsing

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 ﻿window.EVENTS = [
-  { slug:"bumname", title:"bumName"", date:"", category:"events", folder:"bumname" },
-{ slug:"beale-afb-airshow", title:"Beale AFB Airshow", date:"2025-10-14", category:"events", folder:"beale-afb-airshow" },
+  { slug:"bunname", title:"bumName", date:"", category:"events", folder:"bunname" },
+  { slug:"beale-afb-airshow", title:"Beale AFB Airshow", date:"2025-10-14", category:"events", folder:"beale-afb-airshow" },
   { slug:"bloom", title:"Bloom", date:"2025-04-14", category:"events", folder:"bloom" },
   { slug:"dc-x-lantern-festival", title:"DC X Lantern Festival", date:"2025-03-30", category:"events", folder:"dc-x-lantern-festival" },
   { slug:"game-mason", title:"Game Mason", date:"2025-09-21", category:"esports", folder:"game-mason" },
@@ -8,5 +8,5 @@
   { slug:"gmu-esports-x-basketball-2026", title:"GMU Esports x GMU Basketball Collab 2026", date:"2026-02-20", category:"esports", folder:"gmu-esports-x-basketball-2026" },
   { slug:"nintendo-night", title:"Nintendo Night", date:"2026-02-21", category:"events", folder:"nintendo-night" },
   { slug:"nyc-2025", title:"NYC 2025", date:"2025-09-22", category:"events", folder:"nyc-2025" },
-  { slug:"philly-lan-02-23-25", title:"Philly LAN â€” Feb 23 2025", date:"2025-02-23", category:"esports", folder:"philly-lan-02-23-25" }
+  { slug:"philly-lan-02-23-25", title:"Philly LAN edited", date:"2025-02-23", category:"esports", folder:"philly-lan-02-23-25" }
 ];
