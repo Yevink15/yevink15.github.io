d5791309b052753757fe84b5ced4a9ff99ab4442
Yevink15 <yevinkariyawasam@gmail.com>
2026-02-25 02:01:49 -0500

    Fix basketball album slug paths

--- data.js
+++ data.js
@@ -4,8 +4,8 @@
   { slug:"dc-x-lantern-festival", title:"DC X Lantern Festival", date:"2025-03-30", category:"events", folder:"dc-x-lantern-festival", count: 96 },
   { slug:"game-mason", title:"Game Mason", date:"2025-09-21", category:"esports", folder:"game-mason", count: 80 },
 
-  { slug:"gmu-esports-x-basketball-2025", title:"GMU Esports x GMU Basketball Collab 2025", date:"2025-04-06", category:"esports", folder:"gmu-esports-x-basketball-2025", count: 60 },
-  { slug:"gmu-esports-x-basketball-2026", title:"GMU Esports x GMU Basketball Collab 2026", date:"2026-02-20", category:"esports", folder:"gmu-esports-x-basketball-2026", count: 60 },
+  { slug:"gmu-esports-x-gmu-basketball-collab-2025", title:"GMU Esports x GMU Basketball Collab 2025", date:"2025-04-06", category:"esports", folder:"gmu-esports-x-basketball-2025", count: 60 },
+  { slug:"gmu-esports-x-gmu-basketball-collab-2026", title:"GMU Esports x GMU Basketball Collab 2026", date:"2026-02-20", category:"esports", folder:"gmu-esports-x-basketball-2026", count: 60 },
 
   { slug:"nintendo-night", title:"Nintendo Night", date:"2026-02-21", category:"events", folder:"nintendo-night", count: 13 },
   { slug:"nyc-2025", title:"NYC 2025", date:"2025-09-22", category:"events", folder:"nyc-2025", count: 70 },
