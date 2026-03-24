de953e9a78bdae787b6f7ab3fcf043e85f781135
Yevink15 <yevinkariyawasam@gmail.com>
2026-03-23 21:43:54 -0400

    fix all album counts

--- data.js
+++ data.js
@@ -1,15 +1,14 @@
 ﻿window.EVENTS = [
-  { slug:"beale-afb-airshow", title:"Beale AFB Airshow", date:"2025-10-14", category:"events", folder:"beale-afb-airshow", count: 50 },
-  { slug:"bloom", title:"Bloom", date:"2025-04-14", category:"events", folder:"bloom", count: 94 },
-  { slug:"dc-x-lantern-festival", title:"DC X Lantern Festival", date:"2025-03-30", category:"events", folder:"dc-x-lantern-festival", count: 96 },
-  { slug:"game-mason", title:"Game Mason", date:"2025-09-21", category:"esports", folder:"game-mason", count: 80 },
-
-  { slug:"gmu-esports-x-gmu-basketball-collab-2025", title:"GMU Esports x GMU Basketball Collab 2025", date:"2025-04-06", category:"esports", folder:"gmu-esports-x-gmu-basketball-collab-2025", count: 60 },
-  { slug:"gmu-esports-x-gmu-basketball-collab-2026", title:"GMU Esports x GMU Basketball Collab 2026", date:"2026-02-20", category:"esports", folder:"gmu-esports-x-gmu-basketball-collab-2026", count: 60 },
-
-  { slug:"nintendo-night", title:"Nintendo Night", date:"2026-02-21", category:"events", folder:"nintendo-night", count: 13 },
-  { slug:"nyc-2025", title:"NYC 2025", date:"2025-09-22", category:"events", folder:"nyc-2025", count: 70 },
+  { slug:"beale-afb-airshow", title:"Beale AFB Airshow", date:"2025-10-14", category:"events", folder:"beale-afb-airshow", count: 25 },
+  { slug:"bloom", title:"Bloom", date:"2025-04-14", category:"events", folder:"bloom", count:89 },
+  { slug:"dc-x-lantern-festival", title:"DC X Lantern Festival", date:"2025-03-30", category:"events", folder:"dc-x-lantern-festival", count: 93 },
+  { slug:"game-mason", title:"Game Mason", date:"2025-09-21", category:"esports", folder:"game-mason", count: 90 },
+  { slug:"gmu-esports-x-gmu-basketball-collab-2025", title:"GMU Esports x GMU Basketball Collab 2025", date:"2025-04-06", category:"esports", folder:"gmu-esports-x-gmu-basketball-collab-2025", count: 32 },
+  { slug:"gmu-esports-x-gmu-basketball-collab-2026", title:"GMU Esports x GMU Basketball Collab 2026", date:"2026-02-20", category:"esports", folder:"gmu-esports-x-gmu-basketball-collab-2026", count: 102 },
+  { slug:"leadership-legion-spring-2026", title:"Leadership Legion Spring 2026", date:"2026-03-21", category:"events", folder:"leadership-legion-spring-2026", count: 34 },
+  { slug:"mizzou-lan", title:"Mizzou LAN", date:"2026-03-14", category:"esports", folder:"mizzou-lan", count: 83 },
+  { slug:"nintendo-night", title:"Nintendo Night", date:"2026-02-21", category:"events", folder:"nintendo-night", count: 14 },
+  { slug:"nyc-2025", title:"NYC 2025", date:"2025-09-22", category:"events", folder:"nyc-2025", count: 95 },
   { slug:"philly-lan-edited", title:"Philly LAN", date:"2025-02-23", category:"esports", folder:"philly-lan-edited", count: 41 },
-  { slug:"mizzou-lan", title:"Mizzou LAN", date:"2026-03-14", category:"esports", folder:"mizzou-lan", count: 36 },
-  { slug:"leadership-legion-spring-2026", title:"Leadership Legion Spring 2026", date:"2026-03-21", category:"events", folder:"leadership-legion-spring-2026", count: 8 },
+  
 ];
